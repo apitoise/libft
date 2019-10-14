53f3e2616a3d0229267187c6504d761daad214cc
Arthur PITOISET <apitoise@e2r11p23.42.fr>
2019-10-14 18:14:21 +0200

    strncmp

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,20 +6,21 @@
 /*   By: apitoise <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 12:17:51 by apitoise          #+#    #+#             */
-/*   Updated: 2019/10/09 13:09:44 by apitoise         ###   ########.fr       */
+/*   Updated: 2019/10/14 18:12:46 by apitoise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *s1, const char *s2, size_t i)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t comp;
-
-	comp = 0;
-	if (i == 0)
-		return (0);
-	while (comp < i - 1 && (s1[comp] == s2[comp]) && s1[comp] && s2[comp])
-		comp++;
-	return (s1[comp] - s2[comp]);
+	while (n && (*s1 || *s2))
+	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		n--;
+		s1++;
+		s2++;
+	}
+	return (0);
 }
